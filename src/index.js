f4af15768e4e8fbb18987c7685d1111ffacdf47b
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-01 11:34:45 +0300

    set the project files as in the course

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { pizzaData } from "./data.js";
 
 function App() {
   return (
     <div>
       <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza</h2>
     </div>
   );
 }
