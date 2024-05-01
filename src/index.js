b5d4f441d1195c53857656a01b44f5ae542acd7d
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-01 13:56:32 +0300

    add display of sold out pizzas

--- src/index.js
+++ src/index.js
@@ -26,6 +26,11 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+
       <ul className="pizzas">
         {pizzaData.map((pizza) => (
           <Pizza key={pizza.name} pizza={pizza} />
@@ -37,12 +42,12 @@ function Menu() {
 
 function Pizza({ pizza }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizza.soldOut ? "sold-out" : ""}`}>
       <img src={pizza.photoName} alt={pizza.name} />
       <div>
         <h3>{pizza.name}</h3>
         <p>{pizza.ingredients}</p>
-        <span>{pizza.price}</span>
+        <span>{pizza.soldOut ? "SOLD OUT" : pizza.price}</span>
       </div>
     </li>
   );
